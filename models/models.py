c9f316060180dd4cb23b809a40bac5806b135847
Ryuhey_iMac <ryuhey0123@gmail.com>
2020-11-09 22:35:19 +0900

    add project table

--- models/models.py
+++ models/models.py
@@ -36,6 +36,21 @@ class User(Base):
         self.timestamp = datetime.datetime.now()
 
 
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False)
+
+    title = Column(String, nullable=False)
+
+    contents = relationship("Content", backref="projects")
+
+    def __init__(self, title: str) -> None:
+        self.title = title
+        self.timestamp = datetime.datetime.now()
+
+
 class Content(Base):
     __tablename__ = "contents"
 
@@ -48,8 +63,12 @@ class Content(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User')
 
-    def __init__(self, user_id: int, input: dict, output: dict):
+    project_id = Column(Integer, ForeignKey('projects.id'))
+    project = relationship('Project')
+
+    def __init__(self, user_id: int, project_id: int, input: dict, output: dict):
         self.user_id = user_id
+        self.project_id = project_id
         self.input = input
         self.output = output
         self.timestamp = datetime.datetime.now()
@@ -69,28 +88,36 @@ if __name__ == "__main__":
     session.add(User(name='alice', password='fuckyou'))
     session.add(User(name='bob', password='thankyou'))
 
+    test_project1 = Project(title='Test Project1')
+    test_project2 = Project(title='Test Project2')
+    session.add(test_project1)
+    session.add(test_project2)
+
     # コンテンツ追加例 ---------------------------------------------------------------------------------------
 
     # まずユーザー名からアドレスをIDを検索する
-    user: User = session.query(User).filter(User.name == 'alice').first()
+    test_user: User = session.query(User).filter(User.name == 'alice').first()
 
     # そのIDを用いてContentインスタンスを追加する
     session.add(Content(
-        user_id=user.id,
+        user_id=test_user.id,
+        project_id=test_project1.id,
         input={'diameter': 1300, 'length': 17.0},
         output={'x': [1, 2, 3], 'y': [3, 4, 5]}
     ))
 
     session.add(Content(
-        user_id=user.id,
+        user_id=test_user.id,
+        project_id=test_project2.id,
         input={'diameter': 1800, 'length': 17.0},
         output={'x': [1, 2, 5], 'y': [3, 4, 5]}
     ))
 
-    user: User = session.query(User).filter(User.name == 'bob').first()
+    test_user: User = session.query(User).filter(User.name == 'bob').first()
 
     session.add(Content(
-        user_id=user.id,
+        user_id=test_user.id,
+        project_id=test_project1.id,
         input={'diameter': 2000, 'length': 17.0},
         output={'x': [1, 2, 3], 'y': [3, 4, 5]}
     ))
@@ -104,5 +131,7 @@ if __name__ == "__main__":
     # user_id を用いてコンテンツを検索し、最新のものを選択
     content: Content = session.query(Content).filter_by(user_id=user.id).order_by(desc('timestamp')).first()
     print(content.input.get('diameter'))
+    project: Project = session.query(Project).filter_by(id=content.project_id).first()
+    print(project.title)
 
     session.close()
