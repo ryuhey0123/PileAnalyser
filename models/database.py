7a889986810c5dc6264f52ee1c3a4e33cc4a32a9
ryuhey_biz <none>
2020-11-11 13:56:55 +0900

    Add SQLite

--- models/database.py
+++ models/database.py
@@ -1,16 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
+SQLITE_DATABASE_URL = 'sqlite:///' \
+    + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db')
+
+POSTGRESQL_DATABASE_URL = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
     'user': 'ryuhey',  # createuser -P ryuhey
-    'password': 'macmac',
+    'password': 'ryuhey',
     'host': 'localhost',
-    'name': 'pile-analyser'  # createdb Test -O ryuhey
+    'name': 'pile-analyser'  # createdb pile-analyser -O ryuhey
 })
 
-ENGINE = create_engine(SQLALCHEMY_DATABASE_URL, echo=False, convert_unicode=True)
+DATABASE_URL = SQLITE_DATABASE_URL
+# DATABASE_URL = POSTGRESQL_DATABASE_URL
+
+ENGINE = create_engine(DATABASE_URL, echo=False, convert_unicode=True)
 
 SessionMaker = sessionmaker(ENGINE)
 Sess: Session = SessionMaker()
