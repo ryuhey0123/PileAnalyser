065083cd7c04c2ebb97a2df3c94580e5c202d59d
Ryuhey_iMac <ryuhey0123@gmail.com>
2020-11-16 00:02:09 +0900

    input OK

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -3,45 +3,61 @@ import { Button, Label, NumericInput, Radio, RadioGroup } from '@blueprintjs/cor
 
 
 function InputForm() {
-  const [modeValue, setModeValue] = useState('non_liner_multi');
-  const [bottomConditionValue, setBottomConditionValue] = useState('pin');
-  const [materialValue, setMaterialValue] = useState('concrete');
+    interface Inputs {
+        [key: string]: any
+    };
 
-  return (
-    <div>
-      <RadioGroup label="解析モード" onChange={(e) => setModeValue(e.currentTarget.value)} selectedValue={modeValue}>
-        <Radio label="非線形(多層地盤)" value="non_liner_multi" />
-        <Radio label="非線形(単層地盤)" value="non_liner_single" />
-        <Radio label="線形" value="liner" />
-      </RadioGroup>
-      <RadioGroup label="杭脚条件" onChange={(e) => setBottomConditionValue(e.currentTarget.value)} selectedValue={bottomConditionValue}>
-        <Radio label="ピン" value="pin" />
-        <Radio label="自由" value="free" />
-      </RadioGroup>
-      <RadioGroup label="材質" onChange={(e) => setMaterialValue(e.currentTarget.value)} selectedValue={materialValue}>
-        <Radio label="コンクリート" value="concrete" />
-        <Radio label="鋼材" value="steel" />
-      </RadioGroup>
+    const [modeValue, setModeValue] = useState("");
+    const [btmConditionValue, setBtmConditionValue] = useState("");
+    const [materialValue, setMaterialValue] = useState("");
+    const [inputValues, setInputValues] = useState<Inputs>({});
 
-      <Label>杭頭固定度
-        <NumericInput max={1.0} min={0.0} stepSize={0.001} minorStepSize={0.001} majorStepSize={0.1} />
-      </Label>
-      <Label>杭径(mm)
-        <NumericInput min={0.0} stepSize={10} minorStepSize={10} majorStepSize={100} />
-      </Label>
-      <Label>杭長(m)
-        <NumericInput min={0.0} stepSize={0.1} minorStepSize={0.1} majorStepSize={1} />
-      </Label>
-      <Label>杭天端(m)
-        <NumericInput stepSize={0.1} minorStepSize={0.1} majorStepSize={1} />
-      </Label>
-      <Label>入力水平力(kN)
-        <NumericInput min={0.0} stepSize={10} minorStepSize={10} majorStepSize={100} />
-      </Label>
+    const appendValue = (key: string, value: any) => {
+        inputValues[key] = value;
+        return inputValues
+    };
 
-      <Button intent="primary" icon="tick" text="Solve" />
-    </div>
-  );
+    const onValueChange = (valueAsNumber: number, valueAsString: string, inputElement: HTMLInputElement | null) => {
+        if (inputElement != null) {
+            setInputValues(appendValue(inputElement.id, valueAsNumber))
+        };
+    };
+
+    return (
+        <div>
+        <RadioGroup label="解析モード" onChange={e => setModeValue(e.currentTarget.value)} selectedValue={modeValue}>
+            <Radio id="analysis_mode" label="非線形(多層地盤)" value="non_liner_multi" />
+            <Radio id="analysis_mode" label="非線形(単層地盤)" value="non_liner_single" />
+            <Radio id="analysis_mode" label="線形" value="liner" />
+        </RadioGroup>
+        <RadioGroup label="杭脚条件" onChange={e => setBtmConditionValue(e.currentTarget.value)} selectedValue={btmConditionValue}>
+            <Radio id="bottom_condition" label="ピン" value="pin" />
+            <Radio id="bottom_condition" label="自由" value="free" />
+        </RadioGroup>
+        <RadioGroup label="材質" onChange={e => setMaterialValue(e.currentTarget.value)} selectedValue={materialValue}>
+            <Radio id="material" label="コンクリート" value="concrete" />
+            <Radio id="material" label="鋼材" value="steel" />
+        </RadioGroup>
+
+        <Label>杭頭固定度
+            <NumericInput id="condition" max={1.0} min={0.0} stepSize={0.001} minorStepSize={0.001} majorStepSize={0.1} onValueChange={onValueChange} />
+        </Label>
+        <Label>杭径(mm)
+            <NumericInput id="diameter" min={0.0} stepSize={10} minorStepSize={10} majorStepSize={100} onValueChange={onValueChange} />
+        </Label>
+        <Label>杭長(m)
+            <NumericInput id="length" min={0.0} stepSize={0.1} minorStepSize={0.1} majorStepSize={1} onValueChange={onValueChange} />
+        </Label>
+        <Label>杭天端(m)
+            <NumericInput id="level" stepSize={0.1} minorStepSize={0.1} majorStepSize={1} onValueChange={onValueChange} />
+        </Label>
+        <Label>入力水平力(kN)
+            <NumericInput id="force" min={0.0} stepSize={10} minorStepSize={10} majorStepSize={100} onValueChange={onValueChange} />
+        </Label>
+
+        <Button intent="primary" icon="tick" text="Solve" />
+        </div>
+    );
 }
 
 export default InputForm;
